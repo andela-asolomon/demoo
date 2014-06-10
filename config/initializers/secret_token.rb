cb1981df819e1325a7bd8223a0fc74c43f879905
ayoolasolomon <ayoyink@yahoo.com>
2014-06-10 12:41:23 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoo::Application.config.secret_token = '928ca366f80c7e4216541a43f94d3aac2511c69c9ffc05dc5bc12dfa2d587743de5314db15a1b8f6af3ab41a1dfaf2d3d3090b7b6a8b88cb2ef67ce30a133137'
